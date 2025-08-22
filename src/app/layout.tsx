4830c4e67908b27ac9a4c9be9d82c57eddfacd06
vinod <21r21a3344@mlrinstitutions.ac.in>
2025-08-22 23:26:56 +0530

    fix: SSR hydration issue - forced dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,8 @@ export default function RootLayout({
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="dark"
-                    // enableSystem
+                    forcedTheme="dark"
+                    enableSystem={false}
                     disableTransitionOnChange
                 >
                     {children}
